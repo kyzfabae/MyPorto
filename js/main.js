4fa4bd0f8167c3d6b581c4de90ad29f626bfbd3d
baee <145501268+kyzfabae@users.noreply.github.com>
2024-05-13 18:30:10 +0700

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let nvbar = document.getElementById("nvbar")
+nvbar.style.display = "block"
+function toogle(){
+    if(nvbar.style.display = "none"){
+        nvbar.style.display = "block";
+    }
+    else{
+    }
+}
+
+function remove(){
+    nvbar.style.display = "none";
+}
